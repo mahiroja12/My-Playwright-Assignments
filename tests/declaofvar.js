cc53134cb29994327d0201a29ea466a0b6b0c043
mahiroja12 <68847389+mahiroja12@users.noreply.github.com>
2026-07-08 11:53:52 +0530

    Week1Day1 Assignment

---
+++ tests/declaofvar.js
@@ -0,0 +1,2 @@
+
+var companyName="Autodesk";
---
+++ tests/variables.js
@@ -0,0 +1,30 @@
+
+/* String Datatype */
+
+let firstName = "Roja.R";
+console.log(firstName);
+console.log(typeof firstName);
+
+/* String Datatype */
+
+let companyName = "Autodesk";
+console.log(companyName);
+console.log(typeof companyName);
+
+/* Number Datatype */
+
+let mobileNumber = 9945538719;
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+/* Boolean Datatype */
+
+let isAutomation = true;
+console.log(isAutomation);
+console.log(typeof isAutomation);0
+
+/* Undefined Datatype */
+
+let hasPlaywright; // not defined hence it will be undefined 
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
